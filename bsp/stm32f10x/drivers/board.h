895fb0941d313d351cd1f382b408bf4dd6339800
seblee <ceeu@vip.qq.com>
2018-12-27 11:13:03 +0800

    formate file

--- bsp/stm32f10x/drivers/board.h
+++ bsp/stm32f10x/drivers/board.h
@@ -23,19 +23,19 @@
 /* whether use board external SRAM memory */
 // <e>Use external SRAM memory on the board
 // 	<i>Enable External SRAM memory
-#define STM32_EXT_SRAM          0
+#define STM32_EXT_SRAM 0
 //	<o>Begin Address of External SRAM
 //		<i>Default: 0x68000000
-#define STM32_EXT_SRAM_BEGIN    0x68000000 /* the begining address of external SRAM */
+#define STM32_EXT_SRAM_BEGIN 0x68000000 /* the begining address of external SRAM */
 //	<o>End Address of External SRAM
 //		<i>Default: 0x68080000
-#define STM32_EXT_SRAM_END      0x68080000 /* the end address of external SRAM */
+#define STM32_EXT_SRAM_END 0x68080000 /* the end address of external SRAM */
 // </e>
 
 // <o> Internal SRAM memory size[Kbytes] <8-64>
 //	<i>Default: 64
-#define STM32_SRAM_SIZE         48
-#define STM32_SRAM_END          (0x20000000 + STM32_SRAM_SIZE * 1024)
+#define STM32_SRAM_SIZE 48
+#define STM32_SRAM_END (0x20000000 + STM32_SRAM_SIZE * 1024)
 
 // <<< Use Configuration Wizard in Context Menu >>>
 
@@ -43,7 +43,7 @@
 // #define RT_USING_UART1
 #define RT_USING_UART2
 //  #define RT_USING_UART3
-// #define RT_USING_UART4
+#define RT_USING_UART4
 //#define RT_USING_UART5
 
 void rt_hw_board_init(void);
--- bsp/stm32f10x/usr/threads/DO_thread.c
+++ bsp/stm32f10x/usr/threads/DO_thread.c
@@ -1,36 +1,34 @@
- #include <rtthread.h>
+#include <rtthread.h>
 #include <components.h>
 
- #include "sys_conf.h"
- #include "spi_bsp.h"
+#include "sys_conf.h"
+#include "spi_bsp.h"
 #include "global_var.h"
 #include "Kits_Memory.h"
 
 //#define TEST 1
 
-void DO_thread_entry(void* parameter)
+void DO_thread_entry(void *parameter)
 {
-	unsigned char Tmpbuf[6]={0x7F,0x7F,0x7F,0x7F,0x7F,0x7F};
-	
+	unsigned char Tmpbuf[6] = {0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F};
+
 	drv_spi_init();
-	g_Var_inst.DO[6]|=DO_UPDATE;
+	g_Var_inst.DO[6] |= DO_UPDATE;
 	rt_kprintf("DO_thread\n");
-	while(1)
-	{	
-#ifdef  TEST
-			Write_74HC595(NUM_74HC595,Tmpbuf);	//–¥74HC595		
-#else		
-			if(g_Var_inst.DO[6]==DO_UPDATE)//À¢–¬ºÃµÁ∆˜ ‰≥ˆ
-			{
-					g_Var_inst.DO[6]&=~DO_UPDATE;
-						// ∏¥÷∆ ˝æ›
-					ReverseCopy(Tmpbuf,g_Var_inst.DO,6);
-					rt_kprintf("-----------0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x02%x\n", Tmpbuf[0],Tmpbuf[1],Tmpbuf[2],Tmpbuf[3],Tmpbuf[4],Tmpbuf[5]);
-					Write_74HC595(NUM_74HC595,Tmpbuf);	//–¥74HC595	
-			}			
+	while (1)
+	{
+#ifdef TEST
+		Write_74HC595(NUM_74HC595, Tmpbuf); //ÂÜô74HC595
+#else
+		if (g_Var_inst.DO[6] == DO_UPDATE) //Âà∑Êñ∞ÁªßÁîµÂô®ËæìÂá∫
+		{
+			g_Var_inst.DO[6] &= ~DO_UPDATE;
+			// Â§çÂà∂Êï∞ÊçÆ
+			ReverseCopy(Tmpbuf, g_Var_inst.DO, 6);
+			rt_kprintf("-----------0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x02%x\n", Tmpbuf[0], Tmpbuf[1], Tmpbuf[2], Tmpbuf[3], Tmpbuf[4], Tmpbuf[5]);
+			Write_74HC595(NUM_74HC595, Tmpbuf); //ÂÜô74HC595
+		}
 #endif
-			rt_thread_delay(500);
-
-	}		
+		rt_thread_delay(500);
+	}
 }
-
